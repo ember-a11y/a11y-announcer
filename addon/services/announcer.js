252abfe813e5ccfb0774521e943970238a6b9d65
Robdel12 <robertdeluca19@gmail.com>
2016-01-24 00:56:37 -0600

    Remove mixin and auto add to the comsuing apps router

--- addon/services/announcer.js
+++ addon/services/announcer.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  message: "has loaded",
   announceMessage: null,
   announceTone: 'polite',
+
   announce: function(message, tone) {
     this.setProperties({
       announceMessage: message,
---
+++ app/initializers/add-announcer-to-router.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import Router from '../router';
+
+export default {
+  name: 'add-announcer-to-router',
+  initialize: function(registry, application) {
+    Router.reopen({
+      announcer: Ember.inject.service('announcer'),
+      didTransition: function() {
+        this._super(...arguments);
+
+        Ember.run.later(this, () => {
+          let pageTitle = Ember.$('title').html().trim();
+          let serviceMessage = this.get('announcer.message');
+          let message = `${pageTitle} ${serviceMessage}`;
+
+          this.get('announcer').announce(message, 'polite');
+        }, 100);
+      }
+    });
+  }
+};
