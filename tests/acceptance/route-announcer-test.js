2dd0ebfa27dbeb12708ac4c0483f16edc84a2cb5
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-04-08 20:20:47 +0530

    Upgrade to latest ember using ember-cli-update & use new testing API

--- tests/acceptance/route-announcer-test.js
+++ tests/acceptance/route-announcer-test.js
@@ -1,71 +1,56 @@
-/* jshint expr:true */
-import { run } from '@ember/runloop';
-import $ from 'jquery';
-import {
-  describe,
-  it,
-  beforeEach,
-  afterEach
-} from 'mocha';
-import { expect } from 'chai';
-import startApp from '../helpers/start-app';
+import { module, test } from 'qunit';
+import { click, visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
 
-describe('Acceptance: RouteAnnouncer', function() {
-  var application;
+module('Acceptance | route announcer', function(hooks) {
+  setupApplicationTest(hooks);
 
-  beforeEach(function() {
-    application = startApp();
-    this.service = application.__container__.lookup('service:announcer');
-  });
+  module("setting different aria properties", function(hooks) {
 
-  afterEach(function() {
-    run(application, 'destroy');
-  });
-
-  describe("setting different aria properties", function() {
-    beforeEach(function() {
-      visit('/');
-      return click('.spec-announce-btn');
+    hooks.beforeEach(async function() {
+      await visit('/');
+      return await click('.spec-announce-btn');
     });
 
-    it("sets aria-live to 'assertive'", function() {
-      expect($('.spec-announcer').attr('aria-live')).to.equal('assertive');
+    test("sets aria-live to 'assertive'", function(assert) {
+      assert.dom('.spec-announcer').hasAttribute('aria-live', 'assertive');
     });
 
-    it("sets the content of the div", function() {
-      expect($('.spec-announcer').html().trim()).to.equal('This is a test');
-    });
-  });
-
-  describe("changing the route change message", function() {
-    beforeEach(function() {
-      this.service.set('message', 'has finished loading');
-      return visit('/');
-    });
-
-    it("updates the message", function() {
-      expect($('.spec-announcer').html().trim()).to.equal('Index title has finished loading');
+    test("sets the content of the div", function(assert) {
+      assert.dom('.spec-announcer').hasText('This is a test');
     });
 
   });
 
-  describe("visiting the 'another' route", function() {
-    beforeEach(function() {
-      return visit('/another');
+  module("changing the route change message", function(hooks) {
+    hooks.beforeEach(async function() {
+      this.owner.lookup('service:announcer').set('message', 'has finished loading');
+      return await visit('/');
     });
 
-    it('pushes changes to the div', function() {
-      expect($('.spec-announcer').html().trim()).to.equal('Another title has loaded');
+    test("updates the message", function(assert) {
+      assert.dom('.spec-announcer').hasText( 'Index title has finished loading');
+    });
+
+  });
+
+  module("visiting the 'another' route", function(hooks) {
+    hooks.beforeEach(async function() {
+      return await visit('/another');
+    });
+
+    test('pushes changes to the div', function(assert) {
+      assert.dom('.spec-announcer').hasText('Another title has loaded');
     });
   });
 
-  describe("visiting the 'index' route", function() {
-    beforeEach(function() {
-      return visit('/');
+  module("visiting the 'index' route", function(hooks) {
+    hooks.beforeEach(async function() {
+      return await visit('/');
     });
 
-    it('pushes changes to the div', function() {
-      expect($('.spec-announcer').html().trim()).to.equal('Index title has loaded');
+    test('pushes changes to the div', function(assert) {
+      assert.dom('.spec-announcer').hasText('Index title has loaded');
     });
   });
 
