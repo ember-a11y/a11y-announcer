394fcf97a5eddcc7990eab2577bd6297079434a7
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-04-08 20:40:25 +0530

    ESLint fixes

--- tests/dummy/app/routes/another.js
+++ tests/dummy/app/routes/another.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  title: "Another title"
+  title: 'Another title'
 });
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -2,7 +2,7 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  title: "Index title",
+  title: 'Index title',
   announcer: service('announcer'),
 
   actions: {
